95dd28d70b81030813e908916c20d6a3f30b8274
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-06-30 07:43:27 +0900

    use :ok and :error

--- lib/versions.ex
+++ lib/versions.ex
@@ -10,42 +10,50 @@ defmodule Versions do
   @type lhs :: atom()
   @type rhs :: atom()
 
-  @spec semver(major, minor, patch) :: semiver
+  @spec semver(major, minor, patch) :: {:ok, semiver} | {:error, String.t}
   def semver(major, minor, patch)
     when is_integer(major) and is_integer(minor) and is_integer(patch)
       and major >= 0 and minor >= 0 and patch >= 0 do
-    %Versions{}
-    |> Map.put(:major, major)
-    |> Map.put(:minor, minor)
-    |> Map.put(:patch, patch)
+    versions = %Versions{}
+               |> Map.put(:major, major)
+               |> Map.put(:minor, minor)
+               |> Map.put(:patch, patch)
+    {:ok, versions}
   end
-  def semver(major, minor, patch)
-    when is_integer(major) and is_integer(minor) and is_integer(patch) do
+  def semver(major, minor, patch) when is_integer(major) and is_integer(minor) and is_integer(patch) do
     cond do
-      major < 0 -> raise ArgumentError, message: "major should bigger and equal to 0"
-      minor < 0 -> raise ArgumentError, message: "minor should bigger and equal to 0"
-      patch < 0 -> raise ArgumentError, message: "patch should bigger and equal to 0"
-      true -> raise ArgumentError
+      major < 0 -> {:error, "major should bigger and equal to 0"}
+      minor < 0 -> {:error, "minor should bigger and equal to 0"}
+      patch < 0 -> {:error, "patch should bigger and equal to 0"}
+      true -> {:error, "major, minor and patch should be integer"}
     end
   end
-  def semver(_, _, _), do: raise ArgumentError, message: "major, minor and patch should be integer"
+  def semver(_, _, _), do: {:error, "major, minor and patch should be integer"}
 
-  @spec compare(semiver, semiver) :: lhs | rhs
+  @spec semver!(major, minor, patch) :: semiver | no_return
+  def semver!(major, minor, patch) do
+    case semver(major, minor, patch) do
+      {:ok, versions} -> versions
+      {:error, message} -> raise ArgumentError, message: message
+    end
+  end
+
+  @spec compare(semiver, semiver) :: {:ok, lhs} | {:ok, rhs} | {:error, String.t}
   def compare(%Versions{major: l_major, minor: l_minor, patch: l_patch},
     %Versions{major: r_major, minor: r_minor, patch: r_patch})
       when is_integer(l_major) and is_integer(l_minor) and is_integer(l_patch)
         and is_integer(r_major) and is_integer(r_minor) and is_integer(r_patch) do
     cond do
-      {l_major, l_minor, l_patch} > {r_major, r_minor, r_patch} -> :lhs
-      {l_major, l_minor, l_patch} <= {r_major, r_minor, r_patch} -> :rhs
-      true -> raise ArgumentError, message: "major, minor and patch should be integer"
+      {l_major, l_minor, l_patch} > {r_major, r_minor, r_patch} -> {:ok, :lhs}
+      {l_major, l_minor, l_patch} <= {r_major, r_minor, r_patch} -> {:ok, :rhs}
+      true -> {:error, "major, minor and patch should be integer"}
     end
   end
-  def compare(_lhs, _rhs), do: raise ArgumentError, message: "major, minor and patch should be integer"
+  def compare(_lhs, _rhs), do: {:error, "major, minor and patch should be integer"}
 
   @spec up(semiver, String.t) :: semiver
   def up(%Versions{major: major, minor: _minor, patch: _patch}, "major"), do: Versions.semver major + 1, 0, 0
   def up(%Versions{major: major, minor: minor, patch: _patch}, "minor"), do: Versions.semver major, minor + 1, 0
   def up(%Versions{major: major, minor: minor, patch: patch}, "patch"), do: Versions.semver major, minor, patch + 1
-  def up(_versions, _), do: raise ArgumentError, message: "no elements"
+  def up(_versions, _), do: {:error, "no elements"}
 end
--- test/versions_test.exs
+++ test/versions_test.exs
@@ -3,51 +3,51 @@ defmodule VersionsTest do
 
   describe "generate semiver" do
     test "every value is positive value" do
-      struct = Versions.semver(1, 4, 2)
+      {:ok, struct} = Versions.semver(1, 4, 2)
       assert struct == %Versions{major: 1, minor: 4, patch: 2}
     end
 
     test "every value is zero value" do
-      struct = Versions.semver(0, 0, 0)
+      {:ok, struct} = Versions.semver(0, 0, 0)
       assert struct == %Versions{major: 0, minor: 0, patch: 0}
     end
   end
 
   describe "argument error with negative value" do
-    test "major", do: assert_raise(ArgumentError, "major should bigger and equal to 0", fn -> Versions.semver(-1, 1, 1) end)
-    test "minor", do: assert_raise(ArgumentError, "minor should bigger and equal to 0", fn -> Versions.semver(1, -1, 1) end)
-    test "patch", do: assert_raise(ArgumentError, "patch should bigger and equal to 0", fn -> Versions.semver(1, 1, -1) end)
+    test "major", do: assert_raise(ArgumentError, "major should bigger and equal to 0", fn -> Versions.semver!(-1, 1, 1) end)
+    test "minor", do: assert_raise(ArgumentError, "minor should bigger and equal to 0", fn -> Versions.semver!(1, -1, 1) end)
+    test "patch", do: assert_raise(ArgumentError, "patch should bigger and equal to 0", fn -> Versions.semver!(1, 1, -1) end)
   end
 
   test "argument error with no integer" do
-    assert_raise(ArgumentError, "major, minor and patch should be integer", fn -> Versions.semver("1", "0", "-1") end)
+    assert_raise(ArgumentError, "major, minor and patch should be integer", fn -> Versions.semver!("1", "0", "-1") end)
   end
 
   describe "compare lhs and rhs" do
     test "1.3.9 is smaller than 1.4.2" do
-      {lhs, rhs} = {Versions.semver(1, 3, 9), Versions.semver(1, 4, 2)}
-      assert Versions.compare(lhs, rhs) == :rhs
+      {{:ok, lhs}, {:ok, rhs}} = {Versions.semver(1, 3, 9), Versions.semver(1, 4, 2)}
+      assert Versions.compare(lhs, rhs) == {:ok, :rhs}
     end
 
     test "1.3.9 is smaller than 1.3.9" do
-      {lhs, rhs} = {Versions.semver(1, 3, 9), Versions.semver(1, 3, 9)}
-      assert Versions.compare(lhs, rhs) == :rhs
+        {{:ok, lhs}, {:ok, rhs}} = {Versions.semver(1, 3, 9), Versions.semver(1, 3, 9)}
+      assert Versions.compare(lhs, rhs) == {:ok, :rhs}
     end
 
     test "0.0.2 is bigger than 0.0.1" do
-      {lhs, rhs} = {Versions.semver(0, 0, 2), Versions.semver(0, 0, 1)}
-      assert Versions.compare(lhs, rhs) == :lhs
+        {{:ok, lhs}, {:ok, rhs}} = {Versions.semver(0, 0, 2), Versions.semver(0, 0, 1)}
+      assert Versions.compare(lhs, rhs) == {:ok, :lhs}
     end
   end
 
   describe "version up" do
     setup do
-      sem = Versions.semver(1, 1, 1)
+      {:ok, sem} = Versions.semver(1, 1, 1)
       {:ok, sem: sem}
     end
 
-    test "major", context, do: assert Versions.up(context.sem, "major") == %Versions{major: 2, minor: 0, patch: 0}
-    test "minor", context, do: assert Versions.up(context.sem, "minor") == %Versions{major: 1, minor: 2, patch: 0}
-    test "patch", context, do: assert Versions.up(context.sem, "patch") == %Versions{major: 1, minor: 1, patch: 2}
+    test "major", context, do: assert Versions.up(context.sem, "major") == {:ok, %Versions{major: 2, minor: 0, patch: 0}}
+    test "minor", context, do: assert Versions.up(context.sem, "minor") == {:ok, %Versions{major: 1, minor: 2, patch: 0}}
+    test "patch", context, do: assert Versions.up(context.sem, "patch") == {:ok, %Versions{major: 1, minor: 1, patch: 2}}
   end
 end
